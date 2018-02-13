9dbd0c7bf2c288aef4dbe52111e9399dc1012131
Nacho Sancho <sannacho@gmail.com>
2018-02-13 12:58:19 +0100

    Version 1.0-13

--- usr/share/vitalinux/prerun.d/11-configurar_redes_wireless.sh
+++ usr/share/vitalinux/prerun.d/11-configurar_redes_wireless.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Configuramos la Wireless para las redes que haya por defeto en /opt/pkgs/wireless
 
-/usr/bin/vx-conf-wireless
+##/usr/bin/vx-conf-wireless
+Exec=sudo /sbin/start-stop-daemon --start --quiet -m --name nfs-cliente.sh --pidfile /run/nfs-cliente.pid -b -a /usr/bin/vx-conf-wireless
